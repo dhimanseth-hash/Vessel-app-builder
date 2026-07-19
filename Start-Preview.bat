54a12a2d3ce10bd2c9ead9018af15ca416a822a6
dhimanseth-hash <dhimanseth@gmail.com>
2026-07-19 18:41:40 -0400

    Delete Start-Preview.bat

--- Start-Preview.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Vessel Consistency Checker...
-echo.
-echo Open this URL in your browser:
-echo http://localhost:4173
-echo.
-"C:\Users\Lenovo\.cache\codex-runtimes\codex-primary-runtime\dependencies\python\python.exe" -m http.server 4173
-pause
